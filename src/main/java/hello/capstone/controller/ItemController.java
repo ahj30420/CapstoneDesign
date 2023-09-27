aca98375211475643c9f90c38cc4fa5701a078f1
ahj30420 <ahj30420@naver.com>
2023-09-28 02:22:29 +0900

    9th commit

--- src/main/java/hello/capstone/controller/ItemController.java
+++ src/main/java/hello/capstone/controller/ItemController.java
@@ -1,9 +1,16 @@
 package hello.capstone.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import hello.capstone.dto.Item;
 import hello.capstone.service.ItemService;
@@ -18,11 +25,40 @@ public class ItemController {
 
 	private final ItemService itemService;
 	
+	@Value("${file.dir}")
+	private String fileDir;
+	
 	/*
 	 * 아이템 등록
 	 */
 	@PostMapping("/register")
-	public String ItemRegistration(@ModelAttribute Item item) {
+	public String ItemRegistration(@RequestParam("image") MultipartFile Image,
+								   @RequestParam("shopidx") int shopidx,
+								   @RequestParam("itemname") String itemname,
+								   @RequestParam("cost") int cost,
+								   @RequestParam("salecost") int salecost,
+								   @RequestParam("quantity") int quantity,
+								   @RequestParam("category") String category,
+								   @RequestParam("starttime") Date starttime,
+								   @RequestParam("endtime") Date endtime) throws IllegalStateException, IOException {
+		
+		Item item = new Item();
+		item.setShopidx(shopidx);
+		item.setItemname(itemname);
+		item.setCost(cost);
+		item.setSalecost(salecost);
+		item.setQuantity(quantity);
+		item.setCategory(category);
+		item.setStarttime(starttime);
+		item.setEndtime(endtime);
+		
+		if(!Image.isEmpty()) {
+			String fullPath = fileDir + Image.getOriginalFilename();
+			Image.transferTo(new File(fullPath));
+		}
+		
+		item.setImage(Image.getOriginalFilename());
+		
 		itemService.itemsave(item);
 		return "success";
 	}
--- src/main/java/hello/capstone/dto/Item.java
+++ src/main/java/hello/capstone/dto/Item.java
@@ -21,4 +21,6 @@ public class Item {
 	private Date starttime;
 	private Date endtime;
 	
+	public Item() {}
+	
 }
